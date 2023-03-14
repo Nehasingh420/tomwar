f79dcf64057a3be96ab2cc3c91d4b41ef371b383
unknown <carry420@gmail.com>
2023-03-14 14:38:53 -0400

    file ready

---
+++ war1/src/main/java/org/neha/war1/App.java
@@ -0,0 +1,13 @@
+package org.neha.war1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
